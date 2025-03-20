77fcd2a2e9ab3260af811d19902c50d7de2e81ee
Lauragarbre <lgbrena86@gmail.com>
2025-03-20 09:51:18 +0100

    Corección función posicionar Barco
    He modificado y ordenado las diferentes funciones de la colocación del barco para que sean más legibles, además de poner comentarios explicando. También he puesto dentro del init de la clase tablero, la funcion de crear tablero para os dos tableros que tenemos que crear.

--- juego/clases.py
+++ juego/clases.py
@@ -3,83 +3,76 @@ import random
 import numpy as np
 from variables import *
 from funciones import *
+
 class Tablero:
     def __init__(self, nombre_usuario):
-        self.id_usuario = nombre_usuario,
+        self.id_usuario = nombre_usuario
         #self.tamano = np.full(10,10)
         self.barcos = flota_barcos
-        self.tablero_sin_barcos = np.full((10,10),"O") 
-        self.tablero_flota = np.full((10,10),"")  #máquina + nuestros disparos
+        self.tablero_sin_barcos = Tablero.crear_tablero()
+        self.tablero_flota = Tablero.crear_tablero() # nuestro tablero
         self.contador_vidas = 20
 
-    def crear_tablero(relleno = ""):
-    # genero tablero
-        return np.full((10,10),relleno) 
-    
-    def posicionar_barco(tablero, coordenada_inicial, coordenada_final, relleno = "O"):
-    # posiciono los barcos en el tablero
-        fila_inicial = coordenada_inicial[0]
-        fila_final = coordenada_final[0]
-        columna_inicial = coordenada_inicial[1]
-        columna_final = coordenada_final[1]
-        if (fila_final < 0
-            or fila_final >= tablero.shape[0]
-            or columna_final < 0
-            or columna_final >= tablero.shape[1]):
-            return (tablero, "No se puede posicionar")
+    @staticmethod
+    def crear_tablero(relleno = " "):
+        # genero tablero
+        return np.full((10,10), relleno)
+
+    def posicionar_barcos(self, flota_barcos):
+        #Condicion para que se cree un bucle que pase por todos los valores de la flota 
+        # de barcos hasta que estén todos los barcos colocados
+        for eslora in flota_barcos.values():
+            posicionado = False
+            while(not posicionado):
+                _, resultado = self.posicionar_barco_aleatorio(eslora) # va a la funcion posicionar barco aleatorio
+                print(resultado)
+                posicionado = resultado == "Barco posicionado"
+
+    def posicionar_barco_aleatorio(self, eslora):
+        # Generar coordenada inicial aleatoria randint(0, 9)
+        fila_inicial = random.randint(0, self.tablero_flota.shape[0] - 1)
+        columna_inicial = random.randint(0, self.tablero_flota.shape[1] - 1)
+        orientacion = random.choice(['N', 'E', 'S', 'O'])
+        # Calcular coordenada final según orientación
+        if orientacion == 'N':
+            fila_final = fila_inicial - (eslora - 1)
+            columna_final = columna_inicial
+        elif orientacion == 'E':
+            fila_final = fila_inicial
+            columna_final = columna_inicial + (eslora - 1)
+        elif orientacion == 'S':
+            fila_final = fila_inicial + (eslora - 1)
+            columna_final = columna_inicial
+        else:
+            fila_final = fila_inicial
+            columna_final = columna_inicial - (eslora - 1)
         
-        tablero_resultante = np.copy(tablero)
+        print("Posicionando en: " + str((fila_inicial, columna_inicial)) + ", " + str((fila_final, columna_final)) + " orientacion " + orientacion)
+        #una vez originadas las coordenadas y la orientación de manera aleatoria, 
+        # llamamos a la función posicionar barco con orientación
+        return self.posicionar_barco_con_orientacion((fila_inicial, columna_inicial), (fila_final, columna_final))
 
-        for i in range(fila_inicial,fila_final + 1):
-            for j in range(columna_inicial,columna_final + 1):
-                if (tablero_resultante[i,j] == relleno):
-                    return (tablero, "No se puede posicionar")
-                else:
-                    tablero_resultante[i,j] = relleno
-        return (tablero_resultante, "Posicionado")
-
-    def posicionar_barco_aleatorio(self):
-        for barco, eslora in self.barcos[0].items():
-    # Generar coordenada inicial aleatoria randint(0, 9)
-            fila_inicial = random.randint(0, self.tablero_flota.shape[0] - 1)
-            columna_inicial = random.randint(0, self.tablero_flota.shape[1] - 1)
-            orientacion = random.choice(['N', 'E', 'S', 'O'])
-    # Calcular coordenada final según orientación
-            if orientacion == 'N':
-                fila_final = fila_inicial - (eslora - 1)
-                columna_final = columna_inicial
-            elif orientacion == 'E':
-                fila_final = fila_inicial
-                columna_final = columna_inicial + (eslora - 1)
-            elif orientacion == 'S':
-                fila_final = fila_inicial + (eslora - 1)
-                columna_final = columna_inicial
-            else:
-                fila_final = fila_inicial
-                columna_final = columna_inicial - (eslora - 1)
-    
-            print("Posicionando en: " + str((fila_inicial, columna_inicial)) + ", " + str((fila_final, columna_final)) + " orientacion " + orientacion)
-            return Tablero.posicionar_barco_con_orientacion(self.tablero_flota, (fila_inicial, columna_inicial), (fila_final, columna_final))
-    
-    def posicionar_barco_con_orientacion(tablero, coordenada_inicial, coordenada_final, relleno = "O"):
-    # posiciono los barcos en el tablero en coordenada_inicial y coordenada_final ambas incluídas con orientacion
+    #Función que sirve para comprobar si las coordenadas de mi barco no se salen del tablero
+    #  y si se pueden posicionar ( comprobar si no hay otro barco ya)
+    def posicionar_barco_con_orientacion(self, coordenada_inicial, coordenada_final, relleno = "O"):
+        # posiciono los barcos en el tablero en coordenada_inicial y coordenada_final ambas incluídas con orientacion
         fila_inicial = coordenada_inicial[0]
         fila_final = coordenada_final[0]
         columna_inicial = coordenada_inicial[1]
         columna_final = coordenada_final[1]
         # Comprobar que la coordenada inicial entra en el tablero
         if (fila_inicial < 0
-            or fila_inicial >= tablero.shape[0]
+            or fila_inicial >= self.tablero_flota.shape[0]
             or columna_inicial < 0
-            or columna_inicial >= tablero.shape[1]):
-            return (tablero, "No se puede posicionar: coordenada inicial fuera del tablero")
+            or columna_inicial >= self.tablero_flota.shape[1]):
+            return (self.tablero_flota, "No se puede posicionar: coordenada inicial fuera del tablero")
         
         # Comprobar que la coordenada final entra en el tablero
         if (fila_final < 0
-            or fila_final >= tablero.shape[0]
+            or fila_final >= self.tablero_flota.shape[0]
             or columna_final < 0
-            or columna_final >= tablero.shape[1]):
-            return (tablero, "No se puede posicionar: coordenada final fuera del tablero")
+            or columna_final >= self.tablero_flota.shape[1]):
+            return (self.tablero_flota, "No se puede posicionar: coordenada final fuera del tablero")
 
         # Obtener recorrido de la orientacion de las filas
         if (fila_inicial < fila_final):
@@ -93,15 +86,22 @@ class Tablero:
         else:
             recorrido_columna = range(columna_inicial, columna_final - 1, -1)
 
-        # Copia del tablero para evitar modificar el original
-        tablero_resultante = np.copy(tablero)
+        # Copia del tablero para evitar modificar el original 
+        #  en caso de que al ir colocando las esloras, haya una que no puedas colocar porque 
+        #  haya ya otro barco anteriormente colocado
+        tablero_resultante = np.copy(self.tablero_flota)
         for i in recorrido_fila:
             for j in recorrido_columna:
                 if (tablero_resultante[i,j] == relleno):
-                    return (tablero, "No se puede posicionar: Coordenadas ocupadas")
+                    return (tablero_resultante, "No se puede posicionar: Coordenadas ocupadas")
                 else:
                     tablero_resultante[i,j] = relleno
-        return (tablero_resultante, "Barco posicionado")
+        self.tablero_flota = tablero_resultante
+        
+        return (self.tablero_flota, "Barco posicionado")
+
+
+
     
     """"
     FUNCION DISPARAR DE MODO ALEATORIO:
